9b71af5bee6c02eede33db2c190a16a4d4dd4bc6
rertyy <107781565+rertyy@users.noreply.github.com>
2024-12-24 12:42:16 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,10 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        match input.trim() {
-            "exit 0" => exit(),
-            "hello" => println!("Hello, world!"),
+        let cmd = input.split_whitespace().collect::<Vec<&str>>();
+        match cmd[0] {
+            "exit" => exit(cmd[1]),
+            "echo" => echo(cmd[1..].join(" ")),
             _ => invalid_command(input),
         }
     }
@@ -22,6 +23,11 @@ fn invalid_command(input: String) {
     println!("{}: command not found", input.trim());
 }
 
-fn exit() {
-    std::process::exit(0);
+fn echo(msg: String) {
+    println!("{}", msg);
+}
+
+fn exit(code: &str) {
+    let code = code.parse::<i32>().unwrap_or(0);
+    std::process::exit(code);
 }
