7c340f964d0323936fea1b4a067473cd2c3a5266
rertyy <107781565+rertyy@users.noreply.github.com>
2024-12-25 10:47:40 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ enum Command {
     Type,
     Invalid,
     External(PathBuf),
+    Pwd,
 }
 
 impl Command {
@@ -16,6 +17,7 @@ impl Command {
             "exit" => Self::Exit,
             "echo" => Self::Echo,
             "type" => Self::Type,
+            "pwd" => Self::Pwd,
             _ => check_path(cmd).map(Self::External).unwrap_or(Self::Invalid),
         }
     }
@@ -27,11 +29,12 @@ impl Command {
             Self::Type => "type",
             Self::Invalid => "invalid",
             Self::External(p) => p.file_name().unwrap().to_str().unwrap(),
+            Self::Pwd => "pwd",
         }
     }
 }
 
-fn check_path(cmd: &str) -> Result<PathBuf, bool> {
+fn check_path(cmd: &str) -> Result<PathBuf, ()> {
     let path_env = std::env::var("PATH").unwrap();
     let paths = path_env.split(":").collect::<Vec<&str>>();
 
@@ -43,7 +46,7 @@ fn check_path(cmd: &str) -> Result<PathBuf, bool> {
         }
     }
 
-    Err(false)
+    Err(())
 }
 
 fn main() {
@@ -57,6 +60,9 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
         let input_vec = input.split_whitespace().collect::<Vec<&str>>();
+        if input_vec.is_empty() {
+            continue;
+        }
 
         let command = Command::from_str(input_vec[0]);
         match command {
@@ -65,8 +71,14 @@ fn main() {
             Command::Type => type_cmd(input_vec[1]),
             Command::External(path) => external_cmd(path, input_vec[1..].to_vec()),
             Command::Invalid => invalid_cmd(&input),
+            Command::Pwd => pwd_cmd(),
         }
     }
+}
+
+fn pwd_cmd() {
+    let current_dir = std::env::current_dir().unwrap();
+    println!("{}", current_dir.display());
 }
 
 fn external_cmd(path: PathBuf, input: Vec<&str>) {
@@ -89,7 +101,7 @@ fn type_cmd(input: &str) {
     match cmd {
         Command::External(path) => println!("{} is {}", input, path.to_str().unwrap()),
         Command::Invalid => println!("{}: not found", input),
-        Command::Exit | Command::Echo | Command::Type => {
+        Command::Exit | Command::Echo | Command::Type | Command::Pwd => {
             println!("{} is a shell builtin", cmd.as_str())
         }
     }
